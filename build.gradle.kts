2511fcfb574bd2dfccabf52a7fccd345ae102977
MaulRai <muhammadraihanofc@gmail.com>
2025-02-11 09:09:07 +0700

    add: ProductTest.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ version = "0.0.1-SNAPSHOT"
 
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion: "5.6.3"
-val junitJupiterVersion: "5.9.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
 
 java {
     toolchain {
