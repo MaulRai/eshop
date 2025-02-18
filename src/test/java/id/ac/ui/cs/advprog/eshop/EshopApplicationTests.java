b7437d5d3a6afa30ac24b7fdd7cba23165d74015
MaulRai <muhammadraihanofc@gmail.com>
2025-02-18 17:55:30 +0700

    done: coverage EshopApplication.java

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -3,6 +3,8 @@ package id.ac.ui.cs.advprog.eshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class EshopApplicationTests {
 
@@ -10,4 +12,10 @@ class EshopApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testMain() {
+        // Verify that the application context loads successfully
+        EshopApplication.main(new String[] {});
+        assertTrue(true, "The application context loaded successfully.");
+    }
 }
