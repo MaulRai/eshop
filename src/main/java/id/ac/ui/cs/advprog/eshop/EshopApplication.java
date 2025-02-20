b9dfba640c1843f3fb12414573af0d3c1ab30258
MaulRai <muhammadraihanofc@gmail.com>
2025-02-20 10:31:37 +0700

    fix: Unused import 'org.springframework.web.bind.annotation.*' at ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("PMD") // Deteksi false positive di PMD
 public class EshopApplication {
 
     public static void main(String[] args) {
