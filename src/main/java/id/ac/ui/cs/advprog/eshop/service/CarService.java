6ffd4e62b50f1e7714a21a96c56c96d323aada12
MaulRai <muhammadraihanofc@gmail.com>
2025-02-25 22:03:41 +0700

    update: adding CarController to ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CarService {
     public Car create(Car car);
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CarServiceImpl implements CarService {
